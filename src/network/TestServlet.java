4dfa10f1063abd6bc44b506e06f653d4ca994e15
Thomas Zhang <thomaszh@usc.edu>
2017-11-19 16:14:35 -0800

    Moved over button which creates socket connection to server.

---
+++ src/network/TestServlet.java
@@ -0,0 +1,64 @@
+package network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String hostname = "localhost";
+		int port = 5000;
+		try {
+			System.out.println("Trying to connect to " + hostname + ":" + port);
+			Socket s = new Socket(hostname, port);
+			System.out.println("Connected to " + hostname + ":" + port);
+			BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			PrintWriter pw = new PrintWriter(s.getOutputStream());
+			pw.println("Donald is checked in.");
+			System.out.println("Checked in.");
+			
+		} catch (IOException ioe) {
+			System.out.println("ioe in Client constructor: " + ioe.getMessage());
+		}
+		
+		PrintWriter out = response.getWriter();
+		out.println("!");
+	}
+
+}
